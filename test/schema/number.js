59699d9e1c7bdd3ac6729d4fd7497895e37ede06
kevin <kevin.leptons@gmail.com>
2019-01-18 11:41:26 +0700

    schema //atom/int, //atom/uint, //atom/float

---
+++ test/schema/number.js
@@ -0,0 +1,41 @@
+const assert = require('assert')
+
+const box = require('../box')
+
+describe('schema://atom/number', () => {
+    let schema_int = '//atom/int'
+    let schema_uint = '//atom/uint'
+    let schema_float = '//atom/float'
+    let dflow
+
+    before(async () => {
+        let service = await box.service()
+        dflow = service.dflow
+    })
+
+    it('verify_int(100)', async () => {
+        dflow.verify(schema_int, 100)
+    })
+
+    it('verify_int(100.1) => error', async () => {
+        assert.throws(() => {
+            dflow.verify(schema_int, 100.1)
+        }, Array)
+    })
+
+    it('verify_uint(100)', async () => {
+        dflow.verify(schema_uint, 100)
+    })
+
+    it('verify_uint(-100) => error', async () => {
+        assert.throws(() => {
+            dflow.verify(schema_uint, -100)
+        }, Array)
+    })
+
+    it('verify_uint(100.1) => error', async () => {
+        assert.throws(() => {
+            dflow.verify(schema_uint, 100.1)
+        }, Array)
+    })
+})
