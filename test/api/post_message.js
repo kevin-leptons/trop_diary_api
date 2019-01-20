5ec2099e58f2c8a9b74043b649a08522d82ffa11
kevin <kevin.leptons@gmail.com>
2019-01-21 02:58:29 +0700

    test on various log level

--- test/api/post_message.js
+++ test/api/post_message.js
@@ -24,6 +24,46 @@ describe('post /message', () => {
         dflow.verify('//trop/front/post_message_res#/body', res.body)
     })
 
+    it('req, level=debug', async () => {
+        res = await req.post(path).
+            send({
+                level: 'debug',
+                message: 'something happens'
+            })
+        assert.equal(res.status, 201)
+        dflow.verify('//trop/front/post_message_res#/body', res.body)
+    })
+
+    it('req, level=warn', async () => {
+        res = await req.post(path).
+            send({
+                level: 'warn',
+                message: 'something happens'
+            })
+        assert.equal(res.status, 201)
+        dflow.verify('//trop/front/post_message_res#/body', res.body)
+    })
+
+    it('req, level=error', async () => {
+        res = await req.post(path).
+            send({
+                level: 'error',
+                message: 'something happens'
+            })
+        assert.equal(res.status, 201)
+        dflow.verify('//trop/front/post_message_res#/body', res.body)
+    })
+
+    it('req, level=fatal', async () => {
+        res = await req.post(path).
+            send({
+                level: 'fatal',
+                message: 'something happens'
+            })
+        assert.equal(res.status, 201)
+        dflow.verify('//trop/front/post_message_res#/body', res.body)
+    })
+
     it('req label=null => 400', async () => {
         res = await req.post(path).
             send({
